c5e25e760c1628c1557235f49060ed355dd7cbec
Andrew Snodgrass <pointlander@gmail.com>
2024-03-30 10:39:23 -0500

    first commit

---
+++ test.c
@@ -0,0 +1,14 @@
+// test.c
+#include <stdio.h>
+extern double __enzyme_autodiff(void*, double);
+double square(double x) {
+    return x * x;
+}
+double dsquare(double x) {
+    // This returns the derivative of square or 2 * x
+    return __enzyme_autodiff((void*) square, x);
+}
+int main() {
+    for(double i=1; i<5; i++)
+        printf("square(%f)=%f, dsquare(%f)=%f\n", i, square(i), i, dsquare(i));
+}
