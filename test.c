a2f77c8831026fe8c4e5da320438ff5f4cb89e7c
Andrew Snodgrass <pointlander@gmail.com>
2024-03-30 22:17:19 -0500

    Added array sorting

--- test.c
+++ test.c
@@ -1,21 +1,58 @@
 // test.c
 #include <stdio.h>
 #include <stdlib.h>
-extern double __enzyme_autodiff(void*, double*, double*);
-double square(double* array) {
-    return *array * *array;
+extern double __enzyme_autodiff(void*, double*, double*, size_t);
+double square(double* arr, size_t n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = arr[i] * arr[i];
+    }
+    int i, j;
+    int swapped = 0;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                double s = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = s;
+                swapped = 1;
+            }
+        }
+ 
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (swapped == 0)
+            break;
+    }
+    double sum = 0;
+    for (int i = 0; i < n; i++ ) {
+        double cost = (double)i - arr[i];
+        if (cost<0) {
+            cost = -cost;
+        }
+        sum += cost;
+    }
+    return sum;
 }
 double dsquare(double* ii, double* shadow) {
     // This returns the derivative of square or 2 * x
-    return __enzyme_autodiff((void*) square, ii, shadow);
+    return __enzyme_autodiff((void*) square, ii, shadow, 10);
 }
 int main() {
-    double* ii = (double*)malloc(8);
-    double* shadow = (double*)malloc(8);
+    double* ii = (double*)malloc(10*8);
+    double* shadow = (double*)malloc(10*8);
     for(double i=1; i<5; i++) {
-        *ii = i;
-        *shadow = 0;
+        for(double j=0; j<10; j++) {
+            ii[(int)j] = 10 - j;
+            shadow[(int)j] = 0;
+        }
         dsquare(ii, shadow);
-        printf("square(%f)=%f, dsquare(%f)=%f\n", i, square(ii), i, *shadow);
+        for(double j=0; j<10; j++) {
+            ii[(int)j] = 10 - j;
+        }
+        double tmp = square(ii, 10);
+        for(int j = 0; j<10; j++) {
+            printf("square(%f)=%f, dsquare(%f)=%f\n", i, tmp, i, shadow[j]);
+        }
     }
 }
