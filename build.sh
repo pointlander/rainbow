fc26887b0dc83df3a33d3aa3f6f1fa0634301d83
Andrew Snodgrass <pointlander@gmail.com>
2024-04-07 07:11:54 -0500

    Added thread support

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 /lib/llvm-16/bin/clang main.c -S -emit-llvm -o input.ll -O2 -fno-vectorize -fno-slp-vectorize -fno-unroll-loops
 /lib/llvm-16/bin/opt -load-pass-plugin=../Enzyme/enzyme/build/Enzyme/LLVMEnzyme-16.so -passes=enzyme -o output.ll input.ll -S
-/lib/llvm-16/bin/clang output.ll -lm -O3 -o rainbow
+/lib/llvm-16/bin/clang output.ll -lpthread -lm -O3 -o rainbow
